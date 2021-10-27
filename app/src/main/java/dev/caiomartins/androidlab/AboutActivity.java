7f501f5b3c6d4016c2c9ece97f952ad624f0c3f1
Caio Martins <dev.caiomartins@gmail.com>
2021-10-28 00:05:43 +0100

    Created About activity + layout

---
+++ app/src/main/java/dev/caiomartins/androidlab/AboutActivity.java
@@ -0,0 +1,21 @@
+package dev.caiomartins.androidlab;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        // Hiding Action Bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+}
