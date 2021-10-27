5c698f6b5bbacc81bfe2e4e4e0aee90a2d34d773
Caio Martins <dev.caiomartins@gmail.com>
2021-10-28 00:03:32 +0100

    Created Splash Activity + layout

--- app/src/main/java/dev/caiomartins/androidlab/SplashActivity.java
+++ app/src/main/java/dev/caiomartins/androidlab/SplashActivity.java
@@ -3,9 +3,11 @@ package dev.caiomartins.androidlab;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Splash extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,4 +22,8 @@ public class Splash extends AppCompatActivity {
     }
 
 
+    public void launchMainActivity(View view) {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
 }
