568f73c7fddb4b89f532955d7f6f204c450f8036
Caio Martins <dev.caiomartins@gmail.com>
2021-10-28 23:39:02 +0100

    feature: Created Settings activity and enabled changing background color

--- app/src/main/java/dev/caiomartins/androidlab/MainActivity.java
+++ app/src/main/java/dev/caiomartins/androidlab/MainActivity.java
@@ -1,13 +1,19 @@
 package dev.caiomartins.androidlab;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,32 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+
+
     }
 
     public void launchAboutActivity(View view) {
         Intent intent = new Intent(MainActivity.this, AboutActivity.class);
         startActivity(intent);
+    }
+
+    public void launchSettingsActivity(View view) {
+        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data.hasExtra("colorHex")) {
+                String hex = data.getStringExtra("colorHex");
+
+                LinearLayoutCompat main_ll = findViewById(R.id.main_ll);
+                main_ll.setBackgroundColor(Color.parseColor(hex));
+            }
+        }
     }
 }
---
+++ app/src/main/java/dev/caiomartins/androidlab/SettingsActivity.java
@@ -0,0 +1,44 @@
+package dev.caiomartins.androidlab;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private EditText colorEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        colorEditText = (EditText) findViewById(R.id.settings_et_color);
+
+        // Hiding Action Bar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+
+    public void setBackgroundColor(View view) {
+        String hex = colorEditText.getText().toString();
+
+        if (hex.isEmpty()) {
+            Toast.makeText(getApplicationContext(),"Hex can't be empty!",Toast.LENGTH_SHORT).show();
+        }
+
+        Intent data = new Intent();
+        data.putExtra("colorHex", hex);
+
+        setResult(RESULT_OK, data);
+        super.finish();
+    }
+}
